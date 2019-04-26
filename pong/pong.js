5c4590c816d98852ca1258b907864fef5659d982
ALEX-DAWSON <ktad2001@gmail.com>
2019-04-26 14:56:21 -0400

    Started Project

--- pong/pong.js
+++ pong/pong.js
@@ -1,1 +1,8 @@
-var canvas;
+// --------------------------------------------- Canvas setup
+ var canvas = document.createElement('canvas');
+ canvas.width = 800;
+ canvas.height = 500;
+ var context = canvas.getContext('2d');
+ var mainThing = document.getElementById('main');
+ mainThing.appendChild(canvas);
+ context.fillStyle="#b7b300";
