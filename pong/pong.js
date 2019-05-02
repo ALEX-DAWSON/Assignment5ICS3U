e2cf1dd3b6ee71a6e57a42880fdd1d29032050c0
ALEX-DAWSON <ktad2001@gmail.com>
2019-05-02 12:45:27 -0400

    Added Paddles

--- pong/pong.js
+++ pong/pong.js
@@ -1,8 +1,16 @@
 // --------------------------------------------- Canvas setup
- var canvas = document.createElement('canvas');
- canvas.width = 800;
- canvas.height = 500;
- var context = canvas.getContext('2d');
- var mainThing = document.getElementById('main');
- mainThing.appendChild(canvas);
- context.fillStyle="#b7b300";
+  var canvas = document.createElement('canvas');
+  canvas.width = 800;
+  canvas.height = 500;
+  var context = canvas.getContext('2d');
+  var mainThing = document.getElementById('main');
+  mainThing.appendChild(canvas);
+  context.fillStyle="#b7b300";
+// ----------------------------------------------Paddle setup
+  var leftpaddley = document.createElement('leftpaddley');
+  leftpaddley.width = 8;
+  leftpaddley.height = 50;
+  var rightpaddley = document.createElement('rightpaddley');
+  rightpaddley.width = 8;
+  tightpaddley.height = 50;
+  // -------------------------------------------- Ball setup
