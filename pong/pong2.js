0ce8a743a3842f71674aeed5f13201f901932c08
ALEX-DAWSON <ktad2001@gmail.com>
2019-05-14 11:29:59 -0400

    I think I made it work

--- pong/pong2.js
+++ pong/pong2.js
@@ -1,3 +1,7 @@
+/* -------------------------------------------------
+Author: Alex Dawson
+Description: Pong game
+-------------------------------------------------- */
 // --------------------------------------------------Canvas setup
  var canvas = document.createElement('canvas');
  // VV This needs to be read first by the computer so it can make the canvas VV
@@ -9,7 +13,7 @@
  window.onload = function() {
    document.body.appendChild(canvas);
  };
-// --------------------------------------------------Global Variable Declarations and Initializations
+// --------------------------------------------------Global Variable Initializations
 // ----------Sprites
 var PlayerPaddle = 225;
 var ComputerPaddle = 225;
@@ -20,25 +24,15 @@ var PlayerPaddle_step = -2;
 var ComputerPaddle_step = 2;
 var BallX_step = 5;
 var BallY_step = 5;
-function MovePieces() {
-  PlayerPaddle += PlayerPaddle_step;
-  ComputerPaddle += ComputerPaddle_step;
-  BallX += BallX_step;
-  BallY += BallY_step;
-}
+// ----------Game Functions
 function EraseCanvas() {
   var eraseCanvas = document.createElement('canvas');
   eraseCanvas.width = canvasWidth;
   eraseCanvas.height = canvasHeight;
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvasWidth, canvasHeight)
 }
-
-// --------------------------------------------------Animation
-
-function DisplayFrames() {
-  setInterval (NextFrame , 60);
-}
-function NextFrame () {
-  EraseCanvas();
+function DrawGame() {
   context.beginPath();
   context.fillStyle = "black";
   context.fillRect(0,PlayerPaddle, 8, 50);
@@ -50,6 +44,21 @@ function NextFrame () {
   context.lineWidth = 1;
   context.strokeStyle = "green";
   context.stroke();
+}
+function MovePieces() {
+  PlayerPaddle += PlayerPaddle_step;
+  ComputerPaddle += ComputerPaddle_step;
+  BallX += BallX_step;
+  BallY += BallY_step;
+}
+// --------------------------------------------------Animation
+
+function DisplayFrames() {
+  setInterval (NextFrame , 60);
+}
+function NextFrame () {
+  EraseCanvas();
+  DrawGame();
   MovePieces();
 }
 
