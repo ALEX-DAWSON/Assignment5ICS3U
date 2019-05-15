7520b1b6243a08a28b09f54a75b8d5da8e7bff0c
ALEX-DAWSON <ktad2001@gmail.com>
2019-05-15 10:44:36 -0400

    Made moving paddles and got the ball bouncing off the edges.

--- pong/pong2.js
+++ pong/pong2.js
@@ -20,11 +20,15 @@ var ComputerPaddle = 225;
 var BallX = 400;
 var BallY = 250;
 // ----------Sprite Movement
-var PlayerPaddle_step = -2;
-var ComputerPaddle_step = 2;
+var PlayerPaddle_step = 0;
+var ComputerPaddle_step = 0;
 var BallX_step = 5;
-var BallY_step = 5;
+var BallY_step = -5;
+var DownKey = {};
+window.addEventListener("keydown", function(event) {DownKey[event.keyCode] = true;});
+window.addEventListener("keyup", function(event) {delete DownKey[event.keyCode];});
 // ----------Game Functions
+var keepPlaying = true;
 function EraseCanvas() {
   var eraseCanvas = document.createElement('canvas');
   eraseCanvas.width = canvasWidth;
@@ -50,7 +54,54 @@ function MovePieces() {
   ComputerPaddle += ComputerPaddle_step;
   BallX += BallX_step;
   BallY += BallY_step;
+  for (var Key in DownKey){
+    // ----------Number(Key) assigns ascii number with key to Value
+    var Value = Number(Key);
+    // ----------For down arrow
+    if (Value == 38) {
+      PlayerPaddle -= 5;
+    }
+    // ----------For up arrow
+    else if (Value == 40) {
+      PlayerPaddle += 5;
+    }
+    // ----------Press Q to quit
+    else if (Value == 81 || Value == 113) {
+      keepPlaying = false;
+    }
+    else {}
+  }
+  if (BallY - 10 < ComputerPaddle + 25) {
+    ComputerPaddle_step = 5;
+  }
 }
+function ManagePieces() {
+  if (PlayerPaddle+50>500) {
+    PlayerPaddle = 500 - 50;
+  }
+  if (PlayerPaddle<0) {
+    PlayerPaddle = 0;
+  }
+  if (ComputerPaddle+50>500) {
+    ComputerPaddle = 500 - 50;
+  }
+  if (ComputerPaddle<0) {
+    ComputerPaddle = 0;
+  }
+  if (BallY + 10 + BallY_step > 500) {
+    BallY = 500 - 10;
+    BallY_step =-1*BallY_step;
+  }
+  if (BallY - 10 - BallY_step < 0) {
+    BallY = 10;
+    BallY_step =-1*BallY_step;
+  }
+  // if (BallY > ComputerPaddle + 50 && ComputerPaddle < BallX + 10 && BallX + 10 > 792 && BallX + 10 < 800) {
+  //   BallX += 10;
+  // }
+}
+// --------------------------------------------------
+
 // --------------------------------------------------Animation
 
 function DisplayFrames() {
@@ -59,8 +110,9 @@ function DisplayFrames() {
 function NextFrame () {
   EraseCanvas();
   DrawGame();
+  ManagePieces();
   MovePieces();
 }
 
-// Main Program
+// --------------------------------------------------Main Program
 DisplayFrames();
